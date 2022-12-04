6bd6bc39f79770f45d07a672071e5bc6d1a1e844
IliaNoah <vaguaman86@gmail.com>
2022-12-04 19:38:24 +0330

    finished functionality part for button click

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const container = document.querySelector('.container');
+const btnSignUp = document.querySelector('.bg button');
+
+btnSignUp.addEventListener('click', function() {
+    container.classList.toggle('change');
+});
