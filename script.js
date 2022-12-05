772be4bccb519377c035e5bc04a4d5722daf97c9
IliaNoah <vaguaman86@gmail.com>
2022-12-05 18:22:30 +0330

    refactor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const container = document.querySelector('.container');
-const btnSignUp = document.querySelector('.bg button');
+const btnSignUp = document.querySelector('.banner button');
 
 btnSignUp.addEventListener('click', function() {
     container.classList.toggle('change');
